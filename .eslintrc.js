5fa4d076e07d75ad5b4e8551b273487edfddf3db
Jieun Oh <ohjieun@kakao.com>
2024-06-19 13:36:50 +0900

    Setting: eslint 설정 추가

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  ignorePatterns: ["*.spec.js"],
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+    babelOptions: {
+      configFile: "./.babelrc",
+    },
+  },
+  plugins: ["prettier"],
+  rules: {
+    semi: "warn",
+    "no-underscore-dangle": 0,
+    "func-names": "off",
+    "no-unused-vars": "warn",
+    "no-param-reassign": 0,
+    "import/prefer-default-export": "off",
+    "class-methods-use-this": "off",
+  },
+};
