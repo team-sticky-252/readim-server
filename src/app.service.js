3dea997501ebdca584a1fcc89d8cb781cc43bac3
Pangho04 <dlckdgh741@naver.com>
2024-06-20 15:10:07 +0900

    Feat: readingTime 계산 함수 추가
    - 아티클 본문, 사용자 WPM을 받아서 readingTime을 ms단위로 반환 합니다.

--- src/app.service.js
+++ src/app.service.js
@@ -5,4 +5,25 @@ export class AppService {
   getHello() {
     return "Hello World!";
   }
+
+  getReadingTime(articleBody, userWPM) {
+    const numOfWords = articleBody.split(" ").length;
+    const rawReadingTime = (numOfWords / userWPM) * 60 * 1000;
+    let readingMinute = Math.floor(rawReadingTime / 1000 / 60);
+    let readingSeconds =
+      Math.round((rawReadingTime / 1000 - readingMinute * 60) * 0.1) * 10;
+
+    if (readingSeconds >= 45) {
+      readingMinute += 1;
+      readingSeconds = 0;
+    } else if (readingSeconds >= 15) {
+      readingSeconds = 30;
+    } else {
+      readingSeconds = 0;
+    }
+
+    const readingTimePerMs = readingMinute * 60 * 1000 + readingSeconds * 1000;
+
+    return readingTimePerMs;
+  }
 }
