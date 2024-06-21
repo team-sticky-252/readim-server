f2d3597e001670438859e637a61d40bf5cbfa757
Jieun Oh <ohjieun@kakao.com>
2024-06-21 22:04:13 +0900

    Chore: CORS 문제 해결

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: "http://localhost:5173",
+    methods: "GET",
+  });
+
   await app.listen(3000);
 }
 bootstrap();
