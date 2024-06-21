f489914f14aa42ea9d48caca791bac933392fb58
Jieun Oh <ohjieun@kakao.com>
2024-06-21 15:46:58 +0900

    Feat: 시멘틱 아티클 본문 판별 알고리즘 작성(공통 요소)
    - 시멘틱 태그로 구성된 사이트의 본문 판별 알고리즘 작성
    - 본문 요소 중 불필요한 요소 제거하는 알고리즘 작성
    - 본문 요소의 텍스트 추출 알고리즘 작성
    - Http response 포맷 작성
    - 상황별 API 에러 처리 위한 클래스와 상수 작성
    - 사이트 오픈 그래프 정보 반환 함수 작성

--- src/app.controller.js
+++ src/app.controller.js
@@ -1,10 +1,48 @@
-import { Controller, Dependencies, Get } from "@nestjs/common";
+import { Controller, Dependencies, Get, Bind, Req, Res } from "@nestjs/common";
+
 import { AppService } from "./app.service";
+import { HttpError, HttpStatus } from "./utils/http";
 
 @Controller()
 @Dependencies(AppService)
 export class AppController {
   constructor(appService) {
     this.appService = appService;
+  }
+
+  @Get("/readingTime")
+  @Bind(Req(), Res())
+  async getReadingTime(request, response) {
+    const { url, wpm = 202 } = request.query;
+
+    try {
+      const formattedURL = this.appService.formatHttpURL(url);
+      const { headElement, bodyElement } =
+        await this.appService.getHtmlElement(formattedURL);
+
+      const readingTime = this.appService.getReadingTime(bodyElement, wpm);
+      const siteOpenGraph = this.appService.getSiteOpenGraph(
+        headElement,
+        formattedURL,
+      );
+
+      return response.status(HttpStatus.OK.statusCode).send({
+        status: HttpStatus.OK.statusCode,
+        data: {
+          readingTime,
+          ...siteOpenGraph,
+        },
+      });
+    } catch (error) {
+      if (error instanceof HttpError) {
+        const { statusCode, message } = error.getError();
+        response.status(statusCode).send({ statusCode, message });
+      }
+
+      const { statusCode, message } = HttpStatus.INTERNAR_SERVER_ERROR;
+      response.status(statusCode).send({ statusCode, message });
+    }
+
+    return null;
   }
 }
