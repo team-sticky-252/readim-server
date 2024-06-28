1648ad5bc3d5c0793e24ac923078fb4f0b85c9fe
Pangho04 <dlckdgh741@naver.com>
2024-06-28 23:30:21 +0900

    Chore: 서버 배포 대응 파일 수정 및 추가
    - ignore 폴더 추가 (빌드 폴더).
    - 빌드용 config 파일 추가 (babel, yml)
    - AWS BeansTalk 8080 Port 대응을 위한 로직 수정.
    - 빌드 및 배포 대응을 위한 모듈 및 명령어 수정

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.SERVER_PORT || 3000;
+  const port = process.env.SERVER_PORT || 8080;
   app.enableCors({
     origin: ["http://localhost:5173", "https://testreadim.netlify.app/"],
     methods: "GET",
