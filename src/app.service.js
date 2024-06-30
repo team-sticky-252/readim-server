609d759214bdbeaf5a37c9079ed6a32914ba2287
Jieun Oh <ohjieun@kakao.com>
2024-07-01 00:24:32 +0900

    Refactor: reading time 초 단위 반올림 로직 변경
    - 초 단위를 반올림하는 것에서 그대로 보여주는 것으로 기획 변경

--- src/app.service.js
+++ src/app.service.js
@@ -63,23 +63,9 @@ export class AppService {
 
   calculateReadingTime(articleBody, wpm) {
     const numOfWords = articleBody.split(" ").length;
-    const rawReadingTime = (numOfWords / wpm) * 60 * 1000;
-    let readingMinutes = Math.floor(rawReadingTime / 1000 / 60);
-    let readingSeconds =
-      Math.round((rawReadingTime / 1000 - readingMinutes * 60) * 0.1) * 10;
+    const readingTimeMs = (numOfWords / wpm) * 60 * 1000;
 
-    if (readingSeconds >= 45) {
-      readingMinutes += 1;
-      readingSeconds = 0;
-    } else if (readingSeconds >= 15) {
-      readingSeconds = 30;
-    } else {
-      readingSeconds = 0;
-    }
-
-    const readingTimePerMs = readingMinutes * 60 * 1000 + readingSeconds * 1000;
-
-    return readingTimePerMs;
+    return readingTimeMs;
   }
 
   getSemanticMainContent(bodyElement) {
