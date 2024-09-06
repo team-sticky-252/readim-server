fd069031d65d922253ac078eaa5841caa7d64bbe
Pangho04 <dlckdgh741@naver.com>
2024-09-07 01:33:03 +0900

    Chore: 서버 배포 대응 Puppeteer 설정 추가

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -2,7 +2,5 @@ const { join } = require("path");
 
 module.exports = {
   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-  executablePath:
-    "/opt/render/.cache/puppeteer/chrome/linux-<version>/chrome-linux64/chrome",
-  headless: true,
+  headless: "new",
 };
