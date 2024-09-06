04e78ef776b927ee5cb4d0cca5be0b58d2727300
Pangho04 <dlckdgh741@naver.com>
2024-09-06 20:14:50 +0900

    Feat: Logger 인스턴스 클래스 생성

--- src/app.module.js
+++ src/app.module.js
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ErrorService } from "./common/exceptions/error.service";
+import CustomLoggerService from "./common/customLogger/custom-logger.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, ErrorService],
+  providers: [AppService, ErrorService, CustomLoggerService],
 })
 export class AppModule {}
